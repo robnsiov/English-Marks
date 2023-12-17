47322dd5fe78c7052ed47ea276877b2d187b265a
robnsiov <robnsiov@gmail.com>
2023-12-17 20:38:49 +0330

    fix bugs

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="absolute inset-0 bg-blue-700 flex justify-between items-center flex-col px-5 py-10 overflow-hidden">
         <div className="absolute inset-x-0 top-0 h-[400px] bg-white">
           <svg
-            className="absolute -bottom-[42px] left-0 -right-1"
+            className="absolute top-full left-0 -right-1"
             viewBox="0 0 1536 200"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -14,12 +14,12 @@ const Home = () => {
             <path
               fill="#fff"
               d="M 0 0 C 389.5 0 389.5 110 779 110 L 779 110 L 779 0 L 0 0 Z"
-              stroke-width="0"
+              strokeWidth="0"
             ></path>{" "}
             <path
               fill="#fff"
               d="M 778 110 C 1152 110 1152 200 1526 200 L 1526 200 L 1526 0 L 778 0 Z"
-              stroke-width="0"
+              strokeWidth="0"
             ></path>{" "}
           </svg>
         </div>
